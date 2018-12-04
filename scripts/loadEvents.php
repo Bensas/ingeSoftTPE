bbf87444aa31080c68b61530537f3f9306f748ae
Bensas <juanbensa@gmail.com>
2018-12-04 14:43:12 -0300

    Addded events page to list all events

--- scripts/loadEvents.php
+++ scripts/loadEvents.php
@@ -7,31 +7,19 @@
         'status' => -1
     ];
 
-    $query = $pdo->prepare("SELECT * FROM events");
+    $query = $pdo->prepare("SELECT * FROM events 
+                            JOIN locations
+                            ON events.locationId = locations.iD");
     $query->execute();
     $events = $query->fetchAll(PDO::FETCH_OBJ);
 
-    foreach ($eventData as $key => $value){
-        $eventData->$key = utf8_encode($value);
-    }
-
-    // echo implode(',', $events[0]);
     if($events) {
-        // $output = array_map( 
-        //     function( $key, $val ){
-        //         return array(
-        //             "Location_name" => $key,
-        //             "Link_name" => $val
-        //         );
-        //     }, array_keys( $arr ), $arr );
         $retObj->events = $events;
         $retObj->status = 0;
     } else {
         $retObj->status = 1;
     }
 
-    foreach ($array as $key => $value) {
-        echo "Key: $key; Value: $value\n";
-    }
+    echo json_encode($retObj);
 
 ?>
